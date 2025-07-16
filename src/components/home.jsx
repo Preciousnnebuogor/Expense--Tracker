2c78db0fef843240a87c3085d3472d689cb103ea
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-07-15 20:45:24 -0700

    enabled the button

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,50 +1,107 @@
-export default function Home(){
-    return (
-      <div className="container">
-        <div className="content">
-          <h1 className="param1">Expense Tracker</h1>
-          <div className="section1">
-            <p className="param2">Your balance</p>
-            <h2 className="param3">$ 22,000,000</h2>
-            <div className="displayIncome">
-              <div className="income">
-                <p>Income</p>
-                <h3>$ 20,000</h3>
+import { useState } from "react";
+
+export default function Home() {
+    const [transaction, setTransaction] = useState([])
+  const [currentInput, setCurrentInput] = useState({
+    description: "",
+    amount: "",
+  });
+
+  const handleTransaction = () => {
+   if(!currentInput.description || !currentInput.amount) return;
+   setTransaction((prev)=>[...prev, currentInput])
+   setCurrentInput({description:"", amount:""})
+  }
+
+  
+
+  function handleIncome(){
+    setAddIncome(addIncome)
+  }
+
+  return (
+    <div className="container">
+      <div className="content">
+        <h1 className="param1">Expense Tracker</h1>
+        <div className="section1">
+          <p className="param2">Your balance</p>
+          <h2 className="param3">$ 0,000,000</h2>
+          <div className="displayIncome">
+            <div className="income">
+              <p>Income</p>
+              <h3>$ 0,000</h3>
+            </div>
+            <div className="expenses">
+              <p>Expenses</p>
+              <h3>$ 0,000</h3>
+            </div>
+          </div>
+        </div>
+        <div className="section2">
+          <div className="transaction">
+            <p className="transact-p"> Transactions </p>
+            <div className="transact-border">
+              <p>Title</p>
+              <p>Amount</p>
+            </div>
+
+            <div className="transaction-list">
+              <div className="transaction-item">
+                <p>Groceries</p>
+                <p>- $50</p>
               </div>
-              <div className="expenses">
-                <p>Expenses</p>
-                <h3>$ 30,000</h3>
+              <div className="transaction-item">
+                <p>Salary</p>
+                <p>+ $200</p>
               </div>
             </div>
           </div>
-          <div className="section2">
-            <div className="transaction">
-              <p className="transact-p"> Transactions </p>
-              <div className="transact-border">
-                <p>Title</p>
-                <p>Amount</p>
-              </div>
+
+          <div className="add-transaction">
+            <p className="transact-p2">Add transactions</p>
+            <div className="descript">
+              <p>Description</p>
+              <input
+              type="text"
+              name="description"
+                required
+                onChange={(e) =>
+                  setCurrentInput((prev) => ({
+                    ...prev,
+                    [e.target.name]: e.target.value,
+                  }))
+                }
+                value={currentInput.description}
+                className="input"
+                placeholder="Enter description..."
+              />
             </div>
 
-            <div className="add-transaction">
-              <p className="transact-p2">Add transactions</p>
-              <div className="descript">
-                <p>Description</p>
-                <input className="input" placeholder="Enter description..." />
-              </div>
+            <div className="amount">
+              <p>Amount</p>
+              <input
+              name="amount"
+                type="number"
+                className="input"
+                required
+                onChange={(e) =>
+                  setCurrentInput((prev) => ({
+                    ...prev,
+                    [e.target.name]: e.target.value,
+                  }))
+                }
+                value={currentInput.amount}
+                placeholder="Enter amount..."
+              />
+              <p className="param4">Use negative (-) for expenses</p>
+            </div>
 
-              <div className="amount">
-                <p>Amount</p>
-                <input className="input" placeholder="Enter amount..." />
-                <p className='param4'>Use negative (-) for expenses</p>
-              </div>
-
-              <div className="button">
-                <button>Add Transactions</button>
-              </div>
+            <div className="button">
+              <button onClick={handleTransaction}>Add Transactions</button>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
